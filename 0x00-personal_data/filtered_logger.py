7fa5f92ebf03b77dab93b2b494a39d47cfc688b1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-05 00:40:53 +0800

    Debug 0x02-1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,8 +13,8 @@ Methods:
             str: log message obfuscated
 '''
 from typing import List
-import re
 import logging
+import re
 
 
 def filter_datum(fields: List[str],
@@ -28,12 +28,12 @@ def filter_datum(fields: List[str],
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    '''Redacting Formatter class
+    '''
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    REDACTION = '***'
+    FORMAT = '[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s'
+    SEPARATOR = ';'
 
     def __init__(self, fields):
         '''Initialize the instance'''
@@ -42,8 +42,7 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         '''filter values in incoming log records.'''
-        original_message = super(RedactingFormatter, self).format(record)
-        redacted_message = filter_datum(self.fields,
-                                        self.REDACTION,
-                                        original_message, self.SEPARATOR)
-        return redacted_message
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
