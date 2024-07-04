021fd661cb8a65b4727a331c85818ab20fdb477a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-04 10:47:36 +0800

    Obfuscate log message that contains sensitive data

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Module defines `filter_datum` function.
+
+Methods:
+    filter_datum:
+        Args:
+            fields: a list of strings representing all fields to obfuscate
+            redaction: string representing by what the field will be obfuscated
+            message: a string representing the log line
+            separator: a string representing by which character is separating,
+            all fields in the log line (message)
+        Returns:
+            str: log message obfuscated
+'''
+from re import sub
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    '''returns the log message obfuscated'''
+    return sub('|'.join('(?<={}=)[^{}]+'.format(f, separator) for f in fields),
+               redaction,
+               message)
