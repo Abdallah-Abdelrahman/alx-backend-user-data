c2dbf3fea76a3588d19795b64d35c4d84947be6e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-10 06:31:27 +0800

    Add class `Auth` which's the template for all authentication system

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Module defines `Auth` class'''
+from typing import List
+from flask import request
+
+
+class Auth:
+    '''class definition'''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''returns False'''
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        '''that returns None'''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''that returns None'''
+        return None
