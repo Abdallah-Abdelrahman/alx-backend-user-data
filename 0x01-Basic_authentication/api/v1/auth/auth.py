1a1937276f66fe4b4270a0e22213944b8462961e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-10 09:36:38 +0800

    Debug 0x03-13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,10 +16,13 @@ class Auth:
             path = path[:-1]
         is_excluded = path in [p[:-1] if p[-1] == '/'
                                else p for p in excluded_paths]
-        if re.match(path, ' '.join([p for p in excluded_paths])):
-            # to check agains trailing astrisk (*)
-            # ex: excluded_paths = ["/api/v1/stat*"]
-            return False
+        for ex in excluded_paths:
+            pattern = ex.split('/')[-1]
+
+            if re.search(rf'{pattern}', path):
+                # to check agains trailing astrisk (*)
+                # ex: excluded_paths = ["/api/v1/stat*"]
+                return False
 
         return not is_excluded
 
