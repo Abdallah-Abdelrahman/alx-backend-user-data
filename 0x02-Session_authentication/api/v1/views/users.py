25fcbc6c73262fe137e83f326664ad856883f44a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-11 21:37:50 +0800

    Debug 0x03-0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,6 +30,8 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == 'me' and not request.current_user:
         abort(404)
     user = User.get(user_id)
+    if user_id == 'me' and request.current_user:
+        return jsonify(user.to_json())
     if user is None:
         abort(404)
     return jsonify(user.to_json())
