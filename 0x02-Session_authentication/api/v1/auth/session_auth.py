20393b58a9e0fc6c8a4a4cb29ac5219be0156e41
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-12 09:35:18 +0800

    Add logout functionality

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -29,3 +29,14 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        '''deletes the user session (logout) '''
+        if not request or not self.session_cookie(request):
+            return False
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+        del self.user_id_by_session_id[user_id]
+        return True
