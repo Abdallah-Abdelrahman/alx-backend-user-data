34fe4d141128486fa4a7aa8fa54726ea2871a635
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-12 08:11:43 +0800

    Debug 0x04-3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,6 +24,6 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         '''that returns a User ID based on a Session ID'''
-        if session_id is None or not isinstance(session_id, str):
+        if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
