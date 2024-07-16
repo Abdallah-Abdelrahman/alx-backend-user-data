f5568655af12dd9e90dcaa6c7f85268707b6e79c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-16 17:13:48 +0800

    Suppress `SQLAlchemy` logging

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,9 +33,9 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         '''save the user to the database'''
-        user = User()
-        user.email = email
-        user.hashed_password = hashed_password
+        user = User(email=email,
+                    hashed_password=hashed_password,
+                    session_id=None, reset_token=None)
         self._session.add(user)
         self._session.commit()
         return user
