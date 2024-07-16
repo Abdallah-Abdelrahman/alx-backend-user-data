7c52a9a421f09eb4e2a8cf2a649b2a6bd3536fe2
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-16 17:25:10 +0800

    Debug 0x07-1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+'''DB module
+'''
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -13,32 +13,32 @@ from typing import Dict
 
 
 class DB:
-    """DB class"""
+    '''DB class'''
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        '''Initialize a new DB instance'''
+        self._engine = create_engine('sqlite:///a.db', echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        '''Memoized session object'''
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds new user to database"""
+        '''Adds new user to database'''
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user in the database based on the specified criteria."""
+        '''Find a user in the database based on the specified criteria.'''
         try:
             user = self._session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
@@ -48,7 +48,7 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs: Dict[str, str]):
-        """Update a user in the database based on the specified criteria."""
+        '''Update a user in the database based on the specified criteria.'''
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if not hasattr(User, key):
