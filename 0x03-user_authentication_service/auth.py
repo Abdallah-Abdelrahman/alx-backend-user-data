dd25b0dc9423c1e9e8fdf540e07525526ea06ed2
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-18 02:14:15 +0800

    Add `register_user` method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 '''Module defines `_hash_password` function'''
+from db import DB
+from user import User
 import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
     '''returns bytes is a salted hash of the input password'''
     return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        '''Initialize the instance'''
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''Add new user'''
+        if self._db.find_user_by(email=email):
+            raise ValueError(f'User {email} already exists')
+        psw = _hash_password(password)
+
+        return self._db.add_user(email, str(psw))
