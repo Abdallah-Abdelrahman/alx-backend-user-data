47c5edda8fae8ab7e76b296ed81127dd573fbcfb
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-18 19:57:30 +0800

    Debug 0x01-14

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,6 +61,8 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         '''Returns the corresponding User'''
+        if not session_id:
+            return
         try:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
