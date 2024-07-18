19891de67921872335fec298532ea14c0943422a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-18 18:38:01 +0800

    Implement `/profile` route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,5 +54,15 @@ def logout():
     redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    '''Finds a user'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({'email': user.email})
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
