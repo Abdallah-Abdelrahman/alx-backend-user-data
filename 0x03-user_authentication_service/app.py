a536af0aada5f3b6ccc5ed3c974f5499173cd633
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-18 17:41:40 +0800

    Implement login route `/sessions`

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Module defines `_hash_password` function'''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -23,6 +23,24 @@ def users():
         return jsonify({'email': user.email, 'message': 'user created'}), 200
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    '''Register new user'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            resp = make_response({
+                'email': email,
+                'message': 'logged in'
+            })
+            resp.set_cookie('session_id', session_id)
+            return resp
+    except ValueError:
+        abort(401)
 
 
 if __name__ == '__main__':
