4ce3ba875738d43f1ef77e8a35a94cb8bdd20c29
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-18 18:35:01 +0800

    Add `logout` route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 '''Module defines `_hash_password` function'''
-from flask import Flask, jsonify, request, abort, make_response
-from sqlalchemy.orm.exc import NoResultFound
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -42,6 +41,17 @@ def login():
     })
     resp.set_cookie('session_id', session_id)
     return resp
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    '''Logs user out route (shoutout xD)'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    redirect('/')
 
 
 if __name__ == '__main__':
