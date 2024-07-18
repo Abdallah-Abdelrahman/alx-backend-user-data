b89ad6de4f9d28739e8f0dc74dfe36ef936e8639
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-18 18:58:51 +0800

    Add `update_password`

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,3 +79,13 @@ class Auth:
             return user.reset_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token, password):
+        '''Updates user's password'''
+        try:
+            self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_psw = _hash_password(password)
+        self._db.update_user(hashed_password=hashed_psw, reset_token=None)
