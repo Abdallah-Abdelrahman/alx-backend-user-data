2ef44382a30db0ed2438167bea574fe43332211b
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-18 19:18:26 +0800

    Debug 0x02-18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,9 +83,10 @@ class Auth:
     def update_password(self, reset_token, password):
         '''Updates user's password'''
         try:
-            self._db.find_user_by(reset_token=reset_token)
+            user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
             raise ValueError
 
-        hashed_psw = _hash_password(password)
-        self._db.update_user(hashed_password=hashed_psw, reset_token=None)
+        self._db.update_user(user.id,
+                             hashed_password=_hash_password(password),
+                             reset_token=None)
